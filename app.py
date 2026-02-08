ae15e9f26245dae08f0c4e26c3e9f383e25c9459
fatjonblp <fatjon.etemi@blp-digital.com>
2026-02-08 19:07:20 +0100

    fix app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import sqlite3
 import pandas as pd
 import plotly.express as px
-from datetime import datetime
+from datetime import datetime, timedelta
 
 st.set_page_config(page_title="Hypotheken-Tracker", layout="wide")
 
